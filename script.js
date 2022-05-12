17411f0120d5eee96bd1e30e92c8e4294b3d02e9
Andrew <lewisdarby2626@gmail.com>
2022-05-12 05:52:47 -0700

    finished

--- script.js
+++ script.js
@@ -2,21 +2,20 @@ const password = document.getElementById("password");
 const confirmPassword = document.querySelector(
   'input[name="confirm-password"]'
 );
+const passwordVerify = document.querySelector(".password-verify");
 const submitButton = document.getElementById("confirm-password");
 const form = document.querySelector("form");
 
 form.addEventListener("submit", passwordAuthentication);
 function passwordAuthentication(event) {
-  if (password.value !== confirmPassword.value || password.value && confirmPassword.value === "") {
+  if (password.value !== confirmPassword.value) {
     event.preventDefault();
-    password.classList.add("password-invalid");
-    confirmPassword.classList.add("password-invalid");
-    
+    password.classList.add("password-invalid")
+    confirmPassword.classList.add("password-invalid")
+    passwordVerify.classList.remove("password-verify-hidden");
   } else if (password.value === confirmPassword.value) {
-    password.classList.remove("password-invalid");
-    confirmPassword.classList.remove("password-invalid");
+    password.classList.remove("password-invalid")
+    passwordVerify.classList.add("password-verify-hidden");
   }
 }
 console.log(password.value, confirmPassword.value);
-
-
